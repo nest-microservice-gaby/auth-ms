243a213e2ed1f36cc5435df76a959e0a610353b6
Gaby Gomez <gabril70@gmail.com>
2025-03-19 11:02:19 -0300

    configurar microservicio de autenticación con NATS y establecer variables de entorno

---
+++ src/config/envs.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+    PORT: string;
+    NATS_SERVERS: string[];
+}
+
+const envVarsSchema = joi.object({
+    PORT: joi.number().required().default(3000),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+}).unknown(true);
+
+const { error, value } = envVarsSchema.validate({
+    ...process.env, 
+        NATS_SERVERS: process.env.NATS_SERVERS?.split(','), 
+    
+});
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars : EnvVars = value
+
+export const envs = {
+    port: envVars.PORT, 
+    natsServers: envVars.NATS_SERVERS,
+};      
---
+++ src/config/service.ts
@@ -0,0 +1,2 @@
+export const NATS_SERVICE = 'NATS_SERVICE';
+// Compare this snippet from client-geteway/src/config/index.ts:
